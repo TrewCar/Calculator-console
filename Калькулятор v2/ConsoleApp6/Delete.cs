7a9c35d39fe9516d83e0982699abc56f255c4471
TrewCar <113162792+TrewCar@users.noreply.github.com>
2022-10-16 15:09:02 +0300

    Add files via upload

---
+++ Калькулятор v2/ConsoleApp6/Delete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static partial class ExtensionMethods
+{
+    public static IEnumerable<T> EachDistinct<T>(this IEnumerable<T> source)
+    {
+        if (!source.Any())
+            yield break;
+
+        T prev = source.First();
+        yield return prev;
+        foreach (var item in source)
+        {
+            if (!Equals(item, prev))
+            {
+                yield return item;
+                prev = item;
+            }
+        }
+    }
+}
+
---
+++ Калькулятор v2/ConsoleApp6/sqrt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Sqrt
+    {
+        public string[] sqr(string[] array)
+        {
+            var result = new string[array.Length];
+            for (int g = 0; g < array.Length; g++)
+            {
+                if (array[g] == "sqrt")
+                {
+                    double a = double.Parse(array[g + 1]);
+                    try
+                    {
+                        double c = double.Parse(array[g + 2]);
+
+                        double b = Math.Pow(c, 1 / a);
+                        array[g] = Convert.ToString(b);
+                        array[g + 1] = Convert.ToString(b);
+                        array[g + 2] = Convert.ToString(b);
+                    }
+                    catch
+                    {
+                        double b = Math.Sqrt(a);
+                        array[g] = Convert.ToString(b);
+                        array[g + 1] = Convert.ToString(b);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
